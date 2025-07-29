1d2e3f5d5a82d47cc27a09429a4c266106544131
Svanyova Adrian <adriansvanyova5@gmail.com>
2025-07-29 02:07:37 +0200

    fasz

---
+++ lua/plugins/init.lua
@@ -0,0 +1,108 @@
+return {
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.treesitter")
+        end,
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require("configs.mason-lspconfig")
+        end,
+    },
+
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.lint")
+        end,
+    },
+
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require("configs.mason-lint")
+        end,
+    },
+
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        config = function()
+            require("configs.conform")
+        end,
+    },
+
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
+    },
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            { "tpope/vim-dadbod", lazy = true },
+            {
+                "kristijanhusak/vim-dadbod-completion",
+                ft = { "sql", "mysql", "plsql" },
+                lazy = true,
+            },
+        },
+        cmd = {
+            "DBUI",
+            "DBUIToggle",
+            "DBUIAddConnection",
+            "DBUIFindBuffer",
+        },
+        init = function()
+            local homedir = os.getenv("HOME")
+            local dbpath = homedir
+                .. "/Documents/Programming/koordi-backend/kordi.db"
+
+            vim.g.dbs = {
+                -- alias = "<dialect>:///<absolute-path>"
+                kordi = "sqlite3:///" .. dbpath,
+            }
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    },
+    { -- optional saghen/blink.cmp completion source
+        "saghen/blink.cmp",
+        opts = {
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+                per_filetype = {
+                    sql = { "snippets", "dadbod", "buffer" },
+                },
+                -- add vim-dadbod-completion to your completion providers
+                providers = {
+                    dadbod = {
+                        name = "Dadbod",
+                        module = "vim_dadbod_completion.blink",
+                    },
+                },
+            },
+        },
+    },
+}
